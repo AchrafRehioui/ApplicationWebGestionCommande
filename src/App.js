54a107692059d7f0e87e12b7d70e0ed3e211447e
HDA <achraf.rehioui.1@gmail.com>
2020-09-29 21:35:17 +0200

    Commencer l'implementation du container BurgerPreparation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
-
+import BurgerPreparateur from './containers/BurgerPreparateur/BurgerPreparateur';
 
 class App extends Component {
   render() {
     return (
       <div >
           <Layout>
-              <p>Test</p>
+             <BurgerPreparateur />
           </Layout> 
       </div>
     );
---
+++ src/containers/BurgerPreparateur/BurgerPreparateur.js
@@ -0,0 +1,15 @@
+ import React, { Component } from 'react';
+import Auxi from '../../hoc/Auxi';
+
+ class BurgerPreparateur extends Component {
+     render () {
+         return (
+             <Auxi>
+                 <div>Burger</div>
+                 <div>Construire des controles</div>
+             </Auxi>
+         );
+     }
+ }
+
+ export default BurgerPreparateur;
