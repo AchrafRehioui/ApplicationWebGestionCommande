d80ba86745249670901b0590373e1cbc4a40c23e
HDA <achraf.rehioui.1@gmail.com>
2020-09-29 11:43:22 +0200

    Creation Layout suite

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,15 @@
-const { ProgressPlugin } = require("webpack")
 
-const layout = () => (
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    {Props.children}
-    <main>
-      
-    </main>
+import React from 'react';
+import Auxi from '../../hoc/Auxi';
+
+const layout = (props) => (
+   <Auxi>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+             {props.children}
+        </main>
+   </Auxi>
+    
 );
+
+export default layout;
